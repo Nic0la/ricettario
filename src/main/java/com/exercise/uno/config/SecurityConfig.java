66bde3294cccdf3e11e19df74144e915ade46e60
Nic0la <nicolaaprile123@gmail.com>
2025-02-27 11:20:52 +0100

    Funziona!
    Il codice è stato ulteriormente pulito

--- src/main/java/com/exercise/uno/config/SecurityConfig.java
+++ src/main/java/com/exercise/uno/config/SecurityConfig.java
@@ -20,7 +20,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig {
 
-
     private final JwtFilter jwtFilter;
 
     public SecurityConfig(JwtFilter jwtFilter) {
@@ -45,32 +44,10 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public JwtDecoder jwtDecoder() throws Exception {
-//        // Carica la chiave pubblica da un certificato X.509
-//        FileInputStream fis = new FileInputStream("path-to-public-key.pem");
-//        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-//        X509Certificate cert = (X509Certificate) cf.generateCertificate(fis);
-//        RSAPublicKey publicKey = (RSAPublicKey) cert.getPublicKey();
-//
-//        // Crea un decoder con la chiave pubblica
-//        return NimbusJwtDecoder.withPublicKey(publicKey).build();
-//    }
-
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        // Specifica l'URL dell'endpoint JWKS
-//        String jwkSetUri = "https://your-provider.com/.well-known/jwks.json"; // Sostituisci con il tuo URL
-//
-//        // Restituisci un JwtDecoder che usa l'issuer e recupera la chiave pubblica dal JWKS
-//        return JwtDecoders.fromIssuerLocation(jwkSetUri);
-//    }
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepo){
@@ -81,58 +58,6 @@ public class SecurityConfig {
         };
     }
 
-//    // Definisci un AuthenticationManager come bean
-//    @Bean
-//    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-//        AuthenticationManagerBuilder authenticationManagerBuilder =
-//                http.getSharedObject(AuthenticationManagerBuilder.class);
-//        authenticationManagerBuilder.inMemoryAuthentication()
-//                .withUser("user")
-//                .password("{noop}password") // {noop} è un prefisso che indica nessuna codifica
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password("{noop}admin")
-//                .roles("ADMIN");
-//        return authenticationManagerBuilder.build();
-//    }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/**", "/").hasRole("ADMIN")
-//                        .requestMatchers("/api/**").hasRole("USER")
-//                        .requestMatchers("/**", "/login").permitAll()
-//                )
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(jwt -> jwt.decoder(jwtDecoder)
-//                                .jwtAuthenticationConverter(jwtAuthenticationConverter()))
-//                )
-//                .csrf(csrf -> csrf.disable()); // Se usi solo API REST, puoi disabilitare CSRF
-//
-//        return http.build();
-//    }
-
-//    @Bean
-//    @Deprecated
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/recipe/**","/","/**", "/login").permitAll() // Permetti l'accesso a tutte le pagine, incluso il login
-//                        .anyRequest().authenticated() // Richiedi autenticazione per tutte le altre richieste
-//                )
-//                .csrf(csrf -> csrf.disable()); // Disabilita CSRF se hai solo API REST
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-//        converter.setJwtGrantedAuthoritiesConverter(new JwtGrantedAuthoritiesConverter());
-//        return converter;
-//    }
  }
 
 
--- src/main/java/com/exercise/uno/service/RecipeService.java
+++ src/main/java/com/exercise/uno/service/RecipeService.java
@@ -1,6 +1,7 @@
 package com.exercise.uno.service;
 
 import com.exercise.uno.mapper.CategoryMapper;
+import com.exercise.uno.mapper.CycleAvoidingMappingContext;
 import com.exercise.uno.mapper.RecipeMapper;
 import com.exercise.uno.models.dto.CategoryDTO;
 import com.exercise.uno.models.dto.RecipeDTO;
@@ -8,6 +9,7 @@ import com.exercise.uno.models.entity.Category;
 import com.exercise.uno.models.entity.Recipe;
 import com.exercise.uno.repository.CategoryRepository;
 import com.exercise.uno.repository.RecipeRepository;
+import com.exercise.uno.service.exception.EntityNotFoundException;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,6 +38,32 @@ public class RecipeService {
     }
 
 
+    public RecipeDTO getRecipeById(Long id) {
+        Recipe recipe = recipeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("No recipe with this id " +id +" was found"));
+        return RecipeMapper.INSTANCE.toDTO(recipe);
+    }
 
+    public RecipeDTO findRecipeById(Long id) throws IllegalArgumentException, EntityNotFoundException
+    {
+        if(id<=0)
+            throw new IllegalArgumentException(id + " is not a valid id");
+        Optional<Recipe> recipe = recipeRepository.findById(id);
+        if(recipe.isEmpty())
+            throw new EntityNotFoundException("No recipe with this id " +id +" was found");
+        return RecipeMapper.INSTANCE.toDTO(recipe.get());
+    }
+
+    public RecipeDTO findRecipeByName(String name) throws EntityNotFoundException {
+
+        Recipe recipe = recipeRepository.findByName(name);
+        if(recipe==null)
+            throw new EntityNotFoundException("No recipe with this name " +name +" was found");
+        return RecipeMapper.INSTANCE.toDTO(recipe);
+    }
+
+    public List<RecipeDTO> findAllRecipe() {
+        List<Recipe> recipes = recipeRepository.findAll();
+        return RecipeMapper.INSTANCE.toDTOList(recipes);
+    }
 
 }
